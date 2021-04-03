1e190401ee7c35d3bc11ec307847ab852a1f60eb
Sharni Zaugg <sharni.greener@gmail.com>
2021-04-02 20:25:09 -0600

    add countdown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-//create object for each question?
-//create setInterval for timer. set at 1000. -5 for every incorrect answer
-//start button triggers main function for quiz
-//correct answer adds point to overall score 
+
 var startScreen = document.querySelector("#start-screen");
 var startButton = document.querySelector("#start-btn");
 var questionScreen = document.querySelector("#question-screen");
+var timeLeft = document.querySelector("#time-left");
+var time = 75;
+var score = 0;
 var questionIndex = 0;
 var quizQuestions = [{
     question: "How do you write a comment in JS?",
@@ -17,27 +17,27 @@ var quizQuestions = [{
     correct: "<script>"
 },
 {
-    questions: "What is the correct function to generate a random number?",
+    question: "What is the correct function to generate a random number?",
     choices: ["Random()", "Math.random()", "Random.math()", "Math.mix()"],
     correct: "Math.random()"
 },
 {
-    questions: "What is the correct way to create a variable?",
+    question: "What is the correct way to create a variable?",
     choices: ["var = myVariable", "myVariable()", "variable = myVariable", "var myVariable"],
     correct: "var myVariable"
 },
 {
-    questions: "How do you link external JavaScript to HTML?",
+    question: "How do you link external JavaScript to HTML?",
     choices: ["href=''", "src=''", "<a></a>", "<link>"],
     correct: "src=''"
 },
 {
-    questions: "Which array is written correctly?",
+    question: "Which array is written correctly?",
     choices: ["var myArray = {hello, world}", "var myArray = 'hello', 'world'", "var myArray = [1, 2, 3]", "var myArray = (1, 2, 3"],
     correct: "var myArray = [1, 2, 3]"
 },
 {
-    questions: "What is the correct syntax for a 'for loop'?",
+    question: "What is the correct syntax for a 'for loop'?",
     choices: ["for (var i = 0; i < 5; i++)", "for (i = 0)", "for loop (var 1 <= 5)", "for [loop i = 0; i < 5]"],
     correct: "for (var i = 0; i < 5; i++)"
 }
@@ -47,8 +47,20 @@ startButton.addEventListener("click", function(){
     startScreen.setAttribute("style", "display:none");
     questionScreen.removeAttribute("style");
     displayQuestion();
+    countdown();
 })
 
+function countdown(){
+    var timeInterval = setInterval(function(){
+        if (time > 0){
+            timeLeft.textContent = time;
+            time--;
+        } else {
+            alert("Times up!");
+            gameOver();
+        }
+    }, 1000);
+}
 function displayQuestion(){
     var questionTitle = document.querySelector("#question-title");
     var questionList = document.querySelector("#question-list");
@@ -58,13 +70,37 @@ function displayQuestion(){
         var listEl = document.createElement("li");
         listEl.textContent = choice;
         questionList.appendChild(listEl);
-        // listEl.addEventListener("click", checkAnswer);
+        listEl.addEventListener("click", checkAnswer);
     });
 };
 
 function checkAnswer (){
+    if (timeLeft <= 0){
+        return score;
+    } else if (questionIndex < quizQuestions.length){
+        if (quizQuestions[questionIndex].choices.clicked === quizQuestions[questionIndex].correct){
+            var correct = document.createElement("p");
+            correct.textContent = "Correct!"
+            correct.classList.add("correct");
+            questionList.appendChild(correct);
+        
+            score++;
+        } else{
+            time -= 5;
+        }
+        questionIndex++;
+        displayQuestion();
+    } else{
+        return score;
+    }
+    
+
+    
+    
+    
+    
+   
     //create if statement
     //if correct, increment score and question index
     //if incorrect, only increment question index
-
 };
